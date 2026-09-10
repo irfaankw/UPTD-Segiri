a278b666a965941618a391be5a4aa82cc827bcfa
Muhammad Irfan <muhammaddirfan17@gmail.com>
2026-09-10 13:02:06 +0800

    build_files.sh breaksystem

--- build_files.sh
+++ build_files.sh
@@ -5,6 +5,6 @@ npm install
 echo "Building Tailwind CSS..."
 npx tailwindcss -i ./static/src/input.css -o ./static/dist/output.css --minify
 echo "Installing Python dependencies..."
-pip install -r requirements.txt
+pip install --break-system-packages -r requirements.txt
 echo "Collecting static files..."
 python manage.py collectstatic --noinput --clear --ignore="src/*"
