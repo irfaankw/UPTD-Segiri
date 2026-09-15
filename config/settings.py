4545e8e14793d2d0b073e1654c152b44875f0b1a
Muhammad Irfan <muhammaddirfan17@gmail.com>
2026-09-15 09:22:59 +0800

    wip: staticfiles config fix

--- config/settings.py
+++ config/settings.py
@@ -34,8 +34,8 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
-    "cloudinary_storage",  # Storage backend Cloudinary (harus sebelum staticfiles)
     "django.contrib.staticfiles",
+    "cloudinary_storage",  # Storage backend Cloudinary (harus sebelum staticfiles)
     "cloudinary",          # Cloudinary core library
     "complaint",
     "core",
